f68dec495ad12cb031068dc82c13d9d37cd6acc4
WindBreaks <siravichchevayont@gmail.com>
2026-02-25 10:15:53 +0700

    Exam_Setup
    Reskhrces .91 changes

---
+++ Assets/Scripts/Workspace/Assignment/StudentSolution.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+using Debug = AssignmentSystem.Services.AssignmentDebugConsole;
+using System;
+using NUnit.Framework;
+using System.Collections.Generic;
+
+namespace Assignment
+{
+    public class StudentSolution : IAssignment
+    {
+        public void EXAM01_FindWords(string[,] board, string[] words)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
